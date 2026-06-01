75828db39eff8092945027ac09019cbffd8a1991
EvilCommunist <AT2415216@yandex.ru>
2026-06-02 01:58:22 +0300

    Add migration

--- db/migrations/20260601215436_subscriptions.sql
+++ db/migrations/20260601215436_subscriptions.sql
@@ -1,5 +1,17 @@
 -- +goose Up
-SELECT 'up SQL query';
+CREATE TABLE subscriptions (
+    id BIGSERIAL PRIMARY KEY,
+    service_name VARCHAR NOT NULL,
+    price INTEGER NOT NULL CHECK (price >= 0),
+    user_id UUID NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE,
+    CONSTRAINT end_date_after_start CHECK (end_date IS NULL OR end_date >= start_date)
+);
+
+CREATE INDEX idx_subscriptions_user_id ON subscriptions(user_id);
+CREATE INDEX idx_subscriptions_service_name ON subscriptions(service_name);
+CREATE INDEX idx_subscriptions_start_date ON subscriptions(start_date);
 
 -- +goose Down
-SELECT 'down SQL query';
+DROP TABLE subscriptions;
